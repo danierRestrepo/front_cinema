c4e4057bd458e259ddb965fd67e1aa63a818d6dd
danierRestrepo <danierr.ortega@udea.edu.co>
2022-12-06 20:57:40 -0500

    first commit, create react project

---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import logo from '../../assets/img/clapperboard.png';
+
+export const Header = ()=>{
+
+    return (
+        <div>
+            <img src={logo} alt="logo" />
+            <h1>Bienvenido</h1>
+        </div>
+    )
+}
