c038c10724154006789a72e5c5a9e5efcc67c08f
danierRestrepo <danierr.ortega@udea.edu.co>
2022-12-13 22:50:04 -0500

    [feature] Added bootstrap to project to use card and navbar

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,39 +1,48 @@
 import "./Content.css";
 
 import { Card } from "../card/Card";
+import { useEffect, useState } from "react";
 
 export const Content = () => {
-  const movies = [
-    {
-      name: "titanic",
-      description: "Una pelicula de amor",
-      image: "https://picsum.photos/seed/picsum/200/300",
-    },
-    {
-      name: "terminator",
-      description: "una pelicula de acción",
-      image: "https://picsum.photos/seed/picsum/200/300",
-    },
-    {
-      name: "Avatar",
-      description: "Una pelicula de amor y acción",
-      image: "https://picsum.photos/seed/picsum/200/300",
-    },
-    {
-      name: "El silencio de los inocentes",
-      description: "Una pelicula de crimen",
-      image: "https://picsum.photos/seed/picsum/200/300",
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {    
+    //getMovies();
+    getMoviesAsync();
+  }, []);
+
+  const getMovies = () => {
+    console.log(1);
+    fetch("http://localhost:8080/api/movie")
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(`2`, 2);
+        //console.log(response);
+        setMovies(response);
+      });
+    console.log(`3`, 3);
+  };
+
+  const getMoviesAsync = async () => {
+    let response = await fetch("http://localhost:8080/api/movie");
+    response = await response.json();    
+    setMovies(response);    
+  };
 
   return (
-    <div className="flex">
+    <div className="row">
       {movies.map((movie, idx) => (
-        <Card
+        <Card          
           key={idx}
           name={movie.name}
-          description={movie.description}
-          image={movie.image}
+          description={!movie.description?"No hay descripción":movie.description}
+          staffList={movie.staffList}
+          image={
+            !movie.imageLink
+              ? "https://picsum.photos/seed/picsum/200/300"
+              : movie.imageLink          
+          }
+          id={movie.id}
         />
       ))}
     </div>
